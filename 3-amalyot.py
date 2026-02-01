5f655fff036b8527182c01cd283471b368060155
Muhammedov Sevinchbek <minecraftuzbektilida@gmail.com>
2026-02-01 15:46:02 +0500

    Refactor square root program for better flow

--- 3-amalyot.py
+++ 3-amalyot.py
@@ -1,15 +1,17 @@
-savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+savol = "Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
 savol += "Musbat son kiriting "
 savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
 
-while savol != 'exit':
+while True:
     qiymat = input(savol)
+    
+    qiymat = float(qiymat)
     if qiymat < 0:
-        print("Iltimos, musbat son kiriting!")
         continue
-    elif qiymat == 'Exit':
-        break
-    else:
-        ildiz = float(qiymat)**(0.5)
 
-print(f"{qiymat} ning ildizi {ildiz} ga teng")
+    elif qiymat == 'exit':
+        break
+    
+    else:
+        ildiz = qiymat ** 0.5
+        print(f"{qiymat} ning ildizi {ildiz} ga teng")
