62882eb02c6287ac81834865fcaab2c045827a72
Muhammedov Sevinchbek <minecraftuzbektilida@gmail.com>
2026-02-03 00:02:29 +0300

    Create 1-misol.py

---
+++ 4-Dars/1-Amalyot/1-misol.py
@@ -0,0 +1,21 @@
+otam = {
+    "Isim" : "Akrom",
+    'Yil' : 1988,
+    "Viloyat" : "Xorazim"
+}
+
+onam = {
+    "Isim" : "Saida",
+    "Yil" : 1988,
+    "Viloyat" : "Xorazim"
+}
+
+ukam = {
+    "Isim" : "Shahzoda",
+    "Yil" : 2013,
+    "Viloyat" : "Xorazim"
+}
+
+print(f"Otamning ismi {otam['Isim']}, {otam['Yil']}-yilda, {otam['Viloyat']} viloyatida tug'ilgan")
+print(f"Onamning ismi {onam['Isim']}, {onam['Yil']}-yilda, {onam['Viloyat']} viloyatida tug'ilgan")
+print(f"Ukamning ismi {ukam['Isim']}, {ukam['Yil']}-yilda, {ukam['Viloyat']} viloyatida tug'ilgan")
